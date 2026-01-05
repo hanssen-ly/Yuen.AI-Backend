2de4a81dab19dffc39bb638672d90031791b1e8c
Hanssen Ly <140537869+hanssen-ly@users.noreply.github.com>
2026-01-05 17:51:58 -0500

    Add getTodayMood to mood routes

--- src/routes/mood.ts
+++ src/routes/mood.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { auth } from "../middleware/auth";
-import { createMood } from "../controllers/moodController";
+import { createMood, getTodayMood } from "../controllers/moodController";
 
 const router = express.Router();
 
